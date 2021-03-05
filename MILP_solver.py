05fb7a7b5081b00ee4206b2eabea61ec231c17e0
Dongtai Du <dudongtai2002@gmail.com>
2021-03-05 04:07:32 -0500

    Minor: some comments add to milp solver

--- MILP_solver.py
+++ MILP_solver.py
@@ -1,10 +1,13 @@
 # With Gurobi+free academic license>=9.0.2 Cvxpy>=1.0.25
 # Note python install gurobi (not included in requirement.txt):
 # pip3 install -i https://pypi.gurobi.com gurobipy
+
+# General solver to solve Mixed Integer Linear Programming problem
 import cvxpy as cp
 
 # Create two scalar optimization variables.
 x = cp.Variable()
+# x = cp.Variable(integer = True) # https://www.cvxpy.org/tutorial/advanced/index.html,
 y = cp.Variable()
 
 # Create constraints.
@@ -19,6 +22,8 @@ obj = cp.Maximize(x*30 + y*20)
 # Form and solve problem.
 prob = cp.Problem(obj, constraints)
 prob.solve()  # Returns the optimal value.
+# prob.solve(solver=cp.GUROBI) #specify solver
+
 print("status:", prob.status)
 print("optimal value", prob.value)
 print("optimal var", x.value, y.value)
